dbcf2706b32d97f078bb0e64a2ca92e6645c3305
bohan2345 <bohan.zheng@gmail.com>
2014-07-03 11:57:23 -0700

    !!

--- src/test/RemoveElementTest.java
+++ src/test/RemoveElementTest.java
@@ -4,20 +4,12 @@ import code.RemoveElement;
 
 public class RemoveElementTest {
 	public static void main(String[] args) {
-		int[] A = { 1, 2, 3, 4 };
-		int[] B = { 1, 2, 3, 4 };
-		int elem = 1;
+		int[] A = { 2, 2, 3 };
+		int elem = 2;
 		RemoveElement test = new RemoveElement();
 		int x = test.removeElement(A, elem);
-		int y = test.removeElementII(B, elem);
 		System.out.println(x);
 		for (int i = 0; i < A.length; i++)
 			System.out.print(A[i]);
-
-		System.out.println("\n---------------------------------");
-
-		System.out.println(y);
-		for (int i = 0; i < B.length; i++)
-			System.out.print(B[i]);
 	}
 }

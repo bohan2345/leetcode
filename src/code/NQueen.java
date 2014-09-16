29ed4e617e2f63b5f7fcca4ddaa5068531237fb1
bohan2345 <bohan.zheng@gmail.com>
2014-09-15 17:30:08 -0700

    nqueen

--- src/code/NQueen.java
+++ src/code/NQueen.java
@@ -5,128 +5,164 @@ import java.util.List;
 
 public class NQueen {
 	public int totalNQueens(int n) {
-		return 0;
+		return solveNQueens(n).size();
 	}
 
 	public List<String[]> solveNQueens(int n) {
 		List<String[]> result = new ArrayList<>();
-		char[][] board = new char[n][n];
-		boolean f = true;
-		for (int row = 0; row < n; row++) {
-			for (int col = 0; col < n; col++) {
-				if (board[row][col] != '.' && board[row][col] != 'Q') {
-					f = placeQueen(board, col, row, n);
-					if (f == false) {
-						break;
+		String[] board = new String[n];
+		for (int i = 0; i < board.length; i++) {
+			StringBuffer sb = new StringBuffer();
+			for (int j = 0; j < n; j++) {
+				sb.append('0');
+			}
+			board[i] = sb.toString();
+		}
+		result.add(board);
+
+		return bfs(result, n);
+	}
+
+	public List<String[]> bfs(List<String[]> result, int N) {
+		List<List<String[]>> results = new ArrayList<List<String[]>>();
+		results.add(result);
+		int x = 0;
+		while (x < N) {
+			List<String[]> newResult = new ArrayList<>();
+			for (int n = 0; n < results.get(x).size(); n++) {
+				String[] board = results.get(x).get(n);
+				for (int i = 0; i < board.length; i++) {
+					if (board[x].charAt(i) == '0') {
+						String[] temp = board.clone();
+						boolean f = placeQueen(temp, i, x, temp.length);
+						if (f) {
+							newResult.add(temp);
+						}
 					}
 				}
 			}
+			x++;
+			results.add(newResult);
 		}
-		return result;
-	}
-	
-	public void dfs(List<String[]> result, char[][] board, char[][] visited){
-		
+		return results.get(x);
 	}
 
-	public boolean placeQueen(char[][] board, int col, int row, int n) {
+	public boolean placeQueen(String[] board, int col, int row, int n) {
 		// column: check is there another queen on this column, set other cell to '.'
 		for (int i = 0; i < n; i++) {
+			StringBuffer sb = new StringBuffer(board[row]);
 			if (i == col) {
-				board[row][col] = 'Q';
+				sb.setCharAt(col, 'Q');
+				board[row] = sb.toString();
 				continue;
 			}
-			if (board[row][i] == 'Q') {
+			if (board[row].charAt(i) == 'Q') {
 				return false;
 			}
-			board[row][i] = '.';
+			sb.setCharAt(i, '.');
+			board[row] = sb.toString();
 		}
 		// row
 		for (int i = 0; i < n; i++) {
+			StringBuffer sb = new StringBuffer(board[i]);
 			if (i == row) {
-				board[row][col] = 'Q';
+				sb.setCharAt(col, 'Q');
+				board[i] = sb.toString();
 				continue;
 			}
-			if (board[i][col] == 'Q') {
+			if (board[i].charAt(col) == 'Q') {
 				return false;
 			}
-			board[i][col] = '.';
+			sb.setCharAt(col, '.');
+			board[i] = sb.toString();
 		}
 		// \
-		int i = col, j = row;
+		int i = row, j = col;
 		while (true) {
 			if (i >= n || j >= n) {
 				break;
 			}
-			if (i == col && j == row) {
-				board[row][col] = 'Q';
+			StringBuffer sb = new StringBuffer(board[i]);
+			if (i == row && j == col) {
+				sb.setCharAt(j, 'Q');
+				board[i] = sb.toString();
 				i++;
 				j++;
 				continue;
 			}
-			if (board[j][i] == 'Q') {
+			if (board[i].charAt(j) == 'Q') {
 				return false;
 			}
-			board[j][i] = '.';
+			sb.setCharAt(j, '.');
+			board[i] = sb.toString();
 			i++;
 			j++;
 		}
 		// \
-		i = col;
-		j = row;
+		i = row;
+		j = col;
 		while (true) {
 			if (i < 0 || j < 0) {
 				break;
 			}
-			if (i == col && j == row) {
-				board[row][col] = 'Q';
+			StringBuffer sb = new StringBuffer(board[i]);
+			if (i == row && j == col) {
+				sb.setCharAt(j, 'Q');
+				board[i] = sb.toString();
 				i--;
 				j--;
 				continue;
 			}
-			if (board[j][i] == 'Q') {
+			if (board[i].charAt(j) == 'Q') {
 				return false;
 			}
-			board[j][i] = '.';
+			sb.setCharAt(j, '.');
+			board[i] = sb.toString();
 			i--;
 			j--;
 		}
 		// /
-		i = col;
-		j = row;
+		i = row;
+		j = col;
 		while (true) {
 			if (i < 0 || j >= n) {
 				break;
 			}
-			if (i == col && j == row) {
-				board[row][col] = 'Q';
+			StringBuffer sb = new StringBuffer(board[i]);
+			if (i == row && j == col) {
+				sb.setCharAt(j, 'Q');
+				board[i] = sb.toString();
 				i--;
 				j++;
 				continue;
 			}
-			if (board[j][i] == 'Q') {
+			if (board[i].charAt(j) == 'Q') {
 				return false;
 			}
-			board[j][i] = '.';
+			sb.setCharAt(j, '.');
+			board[i] = sb.toString();
 			i--;
 			j++;
 		}
-		i = col;
-		j = row;
+		i = row;
+		j = col;
 		while (true) {
 			if (i >= n || j < 0) {
 				break;
 			}
-			if (i == col && j == row) {
-				board[row][col] = 'Q';
+			StringBuffer sb = new StringBuffer(board[i]);
+			if (i == row && j == col) {
+				sb.setCharAt(j, 'Q');
+				board[i] = sb.toString();
 				i++;
 				j--;
 				continue;
 			}
-			if (board[j][i] == 'Q') {
+			if (board[i].charAt(j) == 'Q') {
 				return false;
 			}
-			board[j][i] = '.';
+			sb.setCharAt(j, '.');
+			board[i] = sb.toString();
 			i++;
 			j--;
 		}
--- src/test/NQueenTest.java
+++ src/test/NQueenTest.java
@@ -1,12 +1,36 @@
 package test;
 
+import java.util.List;
+
 import code.NQueen;
 
 public class NQueenTest {
 	public static void main(String[] args) {
 		NQueen test = new NQueen();
-		char[][] board = new char[4][4];
-		test.placeQueen(board, 1, 2, 4);
-		System.out.println(board);
+		// String[] board = { "0000", "0000", "0000", "0000" };
+		// test.placeQueen(board, 1, 2, 4);
+		// for (int i = 0; i < board.length; i++) {
+		// System.out.println(board[i]);
+		// }
+
+		List<String[]> s = test.solveNQueens(7);
+		for (int i = 0; i < s.size(); i++) {
+			String[] ss = s.get(i);
+			for (int j = 0; j < ss.length; j++)
+				System.out.println(ss[j]);
+		}
+
+		// List<String[]> L = new ArrayList<>();
+		// String[] board1 = { "Q...", "....", "....", "...." };
+		// String[] board2 = { "....", "Q...", "....", "...." };
+		// String[] board3 = { "....", "....", "Q...", "...." };
+		// String[] board4 = { "....", "....", "....", "...." };
+		// L.add(board1);
+		// L.add(board2);
+		// L.add(board3);
+		// boolean x = test.listContains(L, board4);
+		// System.out.println(x);
+
+		System.out.println(test.totalNQueens(8));
 	}
 }

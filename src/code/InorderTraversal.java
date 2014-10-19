672630ebecb6ee2aed25ecf53a1fb502ba42d8b5
System Administrator <root@Bohans-MacBook-Pro.local>
2014-10-18 21:28:27 -0700

    lalala

--- src/code/InorderTraversal.java
+++ src/code/InorderTraversal.java
@@ -1,25 +1,56 @@
 package code;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 import test.inorderTraversalTest.TreeNode;
 
 public class InorderTraversal {
 
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-		ArrayList<Integer> vals = new ArrayList<Integer>();
-		if (root != null) {
-			traversal(root, vals);
+//	public ArrayList<Integer> inorderTraversal(TreeNode root) {
+//		ArrayList<Integer> vals = new ArrayList<Integer>();
+//		if (root != null) {
+//			traversal(root, vals);
+//		}
+//		return vals;
+//	}
+//
+//	public void traversal(TreeNode root, ArrayList<Integer> vals) {
+//		if (root != null) {
+//			traversal(root.left, vals);
+//			vals.add(root.val);
+//			traversal(root.right, vals);
+//		} else
+//			return;
+//	}
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> results = new ArrayList<>();
+		if (root == null) {
+			return results;
 		}
-		return vals;
-	}
-
-	public void traversal(TreeNode root, ArrayList<Integer> vals) {
-		if (root != null) {
-			traversal(root.left, vals);
-			vals.add(root.val);
-			traversal(root.right, vals);
-		} else
-			return;
+		Stack<TreeNode> S = new Stack<>();
+		S.push(root);
+		Set<TreeNode> visited = new HashSet<>();
+		while (!S.isEmpty()) {
+			TreeNode node = S.pop();
+			if (visited.contains(node)) {
+				results.add(node.val);
+				continue;
+			}
+			if (node.right != null) {
+				S.push(node.right);
+			}
+			if (node.left == null) {
+				results.add(node.val);
+			} else {
+				S.push(node);
+				S.push(node.left);
+			}
+			visited.add(node);
+		}
+		return results;
 	}
 }
--- src/tc/LearnHashMap.java
+++ src/tc/LearnHashMap.java
@@ -1,7 +1,43 @@
 package tc;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+import test.TreeNode;
+
 public class LearnHashMap extends StaticTest {
 	String name = "subClass name!";
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> results = new ArrayList<>();
+		if (root == null) {
+			return results;
+		}
+		Stack<TreeNode> Q = new Stack<>();
+		Q.push(root);
+		Set<TreeNode> visited = new HashSet<>();
+		while (!Q.isEmpty()) {
+			TreeNode node = Q.pop();
+			if (visited.contains(node)) {
+				results.add(node.val);
+				continue;
+			}
+			if (node.right != null) {
+				Q.push(node.right);
+			}
+			if (node.left == null) {
+				results.add(node.val);
+			} else {
+				Q.push(node.left);
+				Q.push(node);
+			}
+			visited.add(node);
+		}
+		return results;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(new LearnHashMap().name);

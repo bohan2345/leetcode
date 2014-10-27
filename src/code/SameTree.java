df137fffb77217c28078a95ec3d20e1f23c0bce6
bohan2345 <bohan.zheng@gmail.com>
2014-10-27 11:50:28 -0700

    test

--- src/code/SameTree.java
+++ src/code/SameTree.java
@@ -1,63 +1,15 @@
 package code;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import test.TreeNode;
 
 public class SameTree {
-	public class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-
-		TreeNode(int x) {
-			val = x;
-		}
-	}
-
 	public boolean isSameTree(TreeNode p, TreeNode q) {
-		return sameNode(p, q);
-	}
-
-	public boolean sameNode(TreeNode p, TreeNode q) {
-		if (p == null && q == null) {
+		if (p == null && q == null)
 			return true;
-		} else if ((p == null && q != null) || (p != null && q == null)) {
-			return false;
+		if (p != null && q != null) {
+			boolean f = p.val == q.val;
+			return f && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 		}
-		boolean val = (p.val == q.val);
-		boolean left = sameNode(p.left, q.left);
-		boolean right = sameNode(p.right, q.right);
-		if (val && left && right)
-			return true;
-		else
-			return false;
-	}
-
-	public ArrayList<Integer> preorderTraversal(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
-		ArrayList<Integer> vals = new ArrayList<Integer>();
-		Queue<TreeNode> lnodes = new LinkedList<TreeNode>();
-		Stack<TreeNode> rnodes = new Stack<TreeNode>();
-		lnodes.add(root);
-		TreeNode node;
-		while (lnodes.size() != 0 || rnodes.size() != 0) {
-			if (lnodes.size() != 0) {
-				node = lnodes.poll();
-			} else {
-				node = rnodes.pop();
-			}
-			if (node.left != null) {
-				lnodes.add(node.left);
-			}
-			if (node.right != null) {
-				rnodes.push(node.right);
-			}
-			vals.add(node.val);
-		}
-		return vals;
+		return false;
 	}
 }

3692f96d556373af5782372d24c673541d2c8f89
bohan2345 <bohan.zheng@gmail.com>
2014-11-17 14:20:14 -0800

    s

--- src/ctci/Solution_17_3.java
+++ src/ctci/Solution_17_3.java
@@ -1,9 +1,19 @@
 package ctci;
 
 public class Solution_17_3 {
-	public int countZero(int n) {
+	public static int countZero(int n) {
 		int factor = 5;
 		int count = 0;
-		return 1;
+		for (; factor <= n; factor = factor * 5) {
+			for (int i = 1; i <= n; i++) {
+				if (i % factor == 0) 
+					count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(countZero(25));
 	}
 }

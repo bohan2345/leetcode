281e067f48a26af5a7418946c94a2b4e7db607a9
bohan2345 <bohan.zheng@gmail.com>
2015-01-16 17:17:34 -0800

    lalala

--- src/code/AddTwoNumber.java
+++ src/code/AddTwoNumber.java
@@ -3,30 +3,19 @@ package code;
 import test.ListNode;
 
 public class AddTwoNumber {
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		return add(l1, l2, 0);
-	}
-
-	public ListNode add(ListNode l1, ListNode l2, int carry) {
-		ListNode node = null;
-		int x = 0;
-		int c = 0;
-		if (l1 == null && l2 != null) {
-			x = l2.val + carry;
-		} else if (l1 != null && l2 == null) {
-			x = l1.val + carry;
-		} else if (l1 == null && l2 == null) {
-			if (carry == 0)
-				return null;
-			else
-				x = carry;
-		} else {
-			x = l1.val + l2.val + carry;
-		}
-		c = x / 10;
-		x = x % 10;
-		node = new ListNode(x);
-		node.next = add(l1 == null ? null : l1.next, l2 == null ? null : l2.next, c);
-		return node;
-	}
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return add(l1, l2, 0);
+    }
+    
+    ListNode add(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0)
+            return null;
+        int n1 = l1 == null ? 0 : l1.val;
+        int n2 = l2 == null ? 0 : l2.val;
+        int n = n1 + n2 + carry;
+        ListNode node = new ListNode(n % 10);
+        carry = n / 10;
+        node.next = add(l1 == null ? null : l1.next, l2 == null ? null : l2.next, carry);
+        return node;
+    }
 }

72d3567fe2d484cc675930137fc27ac339246481
bohan2345 <bohan.zheng@gmail.com>
2015-01-17 17:05:43 -0800

    lalala

--- src/code/LinkedListCycleII.java
+++ src/code/LinkedListCycleII.java
@@ -1,52 +1,29 @@
 package code;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import test.ListNode;
 
 public class LinkedListCycleII {
-	public ListNode detectCycle(ListNode head) {
-		if (head == null)
-			return null;
-		Set<ListNode> nodes = new HashSet<ListNode>();
-		nodes.add(head);
-		ListNode node = head;
-		while (node.next != null) {
-			node = node.next;
-			if (nodes.contains(node)) {
-				return node;
-			} else {
-				nodes.add(node);
-			}
-		}
-		return null;
-	}
-
-	public ListNode detectCycleII(ListNode head) {
-		if (head == null)
-			return null;
-		ListNode a = head;
-		ListNode b = head;
-		while (a != null && b != null) {
-			a = a.next;
-			b = b.next;
-			if (b != null)
-				b = b.next;
-			else
-				return null;
-			if (a == b)
-				break;
-		}
-		if (b == null)
-			return null;
-		a = head;
-		while (true) {
-			if (a == b)
-				break;
-			a = a.next;
-			b = b.next;
-		}
-		return b;
-	}
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode fast = head, slow = head;
+        while (true) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null)
+                fast = fast.next;
+            if (slow == null || fast == null)
+                return null;
+            if (slow.equals(fast))
+                break;
+        }
+        fast = head;
+        while (true) {
+            if (slow.equals(fast))
+                break;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 }

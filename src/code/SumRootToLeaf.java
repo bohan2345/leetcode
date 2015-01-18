272ddc0ff887e479ca1bf46c0868bfa02a1ee952
bohan2345 <bohan.zheng@gmail.com>
2015-01-18 14:44:15 -0800

    lalala

--- src/code/SumRootToLeaf.java
+++ src/code/SumRootToLeaf.java
@@ -1,31 +1,22 @@
 package code;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import test.TreeNode;
 
 public class SumRootToLeaf {
-	public int sumNumbers(TreeNode root) {
-		if (root == null)
-			return 0;
-		List<Integer> res = new ArrayList<>();
-		dfs(root, res, 0);
-		int sum = 0;
-		for (int i = 0; i < res.size(); i++) {
-			sum += res.get(i);
-		}
-		return sum;
-	}
-
-	public void dfs(TreeNode root, List<Integer> res, int num) {
-		if (root.left == null && root.right == null) {
-			res.add(num * 10 + root.val);
-			return;
-		}
-		if (root.left != null)
-			dfs(root.left, res, num * 10 + root.val);
-		if (root.right != null)
-			dfs(root.right, res, num * 10 + root.val);
-	}
+    public int sumNumbers(TreeNode root) {
+        int[] sum = {0};
+        dfs(root, sum, 0);
+        return sum[0];
+    }
+    
+    void dfs(TreeNode root, int[] sum, int tmp) {
+        if (root == null)
+            return;
+        tmp = tmp * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            sum[0] = sum[0] + tmp;
+        }
+        dfs(root.left, sum, tmp);
+        dfs(root.right, sum, tmp);
+    }
 }

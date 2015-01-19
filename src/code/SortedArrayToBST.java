43b338d9533dbc39d6b7a8e9113c962e50aed5ee
bohan2345 <bohan.zheng@gmail.com>
2015-01-18 23:09:31 -0800

    lalala

--- src/code/SortedArrayToBST.java
+++ src/code/SortedArrayToBST.java
@@ -3,21 +3,18 @@ package code;
 import test.TreeNode;
 
 public class SortedArrayToBST {
-	public TreeNode sortedArrayToBST(int[] num) {
-		if (num.length == 0)
-			return null;
-		return sortedArrayToBST(num, 0, num.length - 1);
-	}
-
-	public TreeNode sortedArrayToBST(int[] num, int first, int last) {
-		if (first > last) {
-			return null;
-		}
-		int mid = (first + last) / 2;
-		int val = num[mid];
-		TreeNode node = new TreeNode(val);
-		node.left = sortedArrayToBST(num, first, mid - 1);
-		node.right = sortedArrayToBST(num, mid + 1, last);
-		return node;
-	}
+    public TreeNode sortedArrayToBST(int[] num) {
+        return build(num, 0, num.length - 1);
+    }
+    
+    TreeNode build(int[] num, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(num[mid]);
+        root.left = build(num, left, mid - 1);
+        root.right = build(num, mid + 1, right);
+        return root;
+    }
 }

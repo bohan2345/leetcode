52590a0c0fd48a57b886799c0e433d292bce6300
bohan2345 <bohan.zheng@gmail.com>
2015-01-19 00:22:47 -0800

    lalala

--- src/code/ConstructBinaryTree.java
+++ src/code/ConstructBinaryTree.java
@@ -3,24 +3,21 @@ package code;
 import test.TreeNode;
 
 public class ConstructBinaryTree {
-	public TreeNode buildTree(int[] preorder, int[] inorder) {
-		return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
-	}
-
-	public TreeNode build(int[] preorder, int pstart, int pend, int[] inorder, int istart, int iend) {
-		if (pstart > pend || istart > iend) {
-			return null;
-		}
-		TreeNode root = new TreeNode(preorder[pstart]);
-		int index = istart;
-		for (int i = istart; i <= iend; i++) {
-			if (preorder[pstart] == inorder[i]) {
-				index = i;
-				break;
-			}
-		}
-		root.left = build(preorder, pstart + 1, pstart + index - istart, inorder, istart, index - 1);
-		root.right = build(preorder, pstart + index - istart + 1, pend, inorder, index + 1, iend);
-		return root;
-	}
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+    
+    TreeNode build(int[] preorder, int preleft, int preright, int[] inorder, int inleft, int inright) {
+        if (preleft > preright)
+            return null;
+        TreeNode root = new TreeNode(preorder[preleft]);
+        int i = inleft;
+        for (; i <= inright; i++) {
+            if (inorder[i] == preorder[preleft])
+                break;
+        }
+        root.left = build(preorder, preleft + 1, preleft + i - inleft, inorder, inleft, i - 1);
+        root.right = build(preorder, preleft + i - inleft + 1, preright, inorder, i + 1, inright);
+        return root;
+    }
 }

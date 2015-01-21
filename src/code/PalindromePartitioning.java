b922608d3052ec3fdc130eb3d79a61155815289b
bohan2345 <bohan.zheng@gmail.com>
2015-01-20 21:52:32 -0800

    lalala

--- src/code/PalindromePartitioning.java
+++ src/code/PalindromePartitioning.java
@@ -4,32 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PalindromePartitioning {
-	public List<List<String>> partition(String s) {
-		List<List<String>> result = new ArrayList<>();
-		if (s == null || s.length() == 0)
-			return result;
-		search(result, new ArrayList<String>(), s, 0);
-		return result;
-	}
-
-	private void search(List<List<String>> result, List<String> tmp, String s, int i) {
-		if (i == s.length()) {
-			result.add(new ArrayList<String>(tmp));
-			return;
-		}
-		for (int end = i + 1; end <= s.length(); end++) {
-			String sub = s.substring(i, end);
-			if (isPalindrome(sub)) {
-				tmp.add(sub);
-				search(result, tmp, s, end);
-				tmp.remove(tmp.size() - 1);
-			}
-		}
-	}
-
-	private boolean isPalindrome(String s) {
-		StringBuilder sb = new StringBuilder(s);
-		sb.reverse();
-		return s.equals(sb.toString());
-	}
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        dfs(s, 0, 0, res, new ArrayList<String>());
+        return res;
+    }
+    
+    void dfs(String s, int start, int end, List<List<String>> res, List<String> tmp) {
+        if (start == s.length()) {
+            res.add(new ArrayList<String>(tmp));
+            return;
+        }
+        for (; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                tmp.add(s.substring(start, end + 1));
+                dfs(s, end + 1, end + 1, res, tmp);
+                tmp.remove(tmp.size() - 1);
+            }
+        }
+    }
+    
+    boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
